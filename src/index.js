6fd9cb2835979b48d635945c7cd69bd4980dd6bf
Brett Grigsby <brett.grigsby@captainu.com>
2018-03-15 20:22:08 -0600

    trying again with better feedback

--- src/index.js
+++ src/index.js
@@ -38,4 +38,4 @@ app.get('/volume', async (req, res) => {
 
 var port = process.env.PORT || 9000;
 
-app.listen(port, () => console.log('Listening on port 9000'))
+app.listen(port, () => console.log(`Listening on port ${port}`))
